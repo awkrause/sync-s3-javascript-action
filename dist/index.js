5ab41d96124a1e96a0b499ecc8773bfd1ab4e842
awkra <awkra@DESKTOP-1LUNAM9>
2020-10-04 16:32:17 +0100

    added logging

--- dist/index.js
+++ dist/index.js
@@ -51,26 +51,33 @@ try {
         }
       };
 
-      var params = {
-        Bucket: bucket,
-        Prefix: prefix
-      };
-    
-      s3.listObjects(params, function(err, data) {
-        if (err) throw err;
-    
-        params = {Bucket: bucketName};
-        params.Delete = {Objects:[]};
-    
-        data.Contents.forEach(function(content) {
-          params.Delete.Objects.push({Key: content.Key});
-        });
-    
-        s3.deleteObjects(params, function(err, data) {
-          if (err) throw err;
-        });
-      });
+      const clearFiles = () => {
+
+        var params = {
+          Bucket: bucket,
+          Prefix: prefix
+        };
       
+        s3.listObjects(params, function(err, data) {
+          if (err) throw err;
+          console.log(`listed files successful.`);
+      
+          params = {Bucket: bucketName};
+          params.Delete = {Objects:[]};
+      
+          data.Contents.forEach(function(content) {
+            params.Delete.Objects.push({Key: content.Key});
+          });
+      
+          s3.deleteObjects(params, function(err, data) {
+            if (err) throw err;
+            console.log(`delete files successful.`);
+          });
+        });
+      };
+
+      
+      clearFiles();
       uploadFile(source);
 
 } 
